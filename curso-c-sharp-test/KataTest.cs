d42b9da6dd638fcb0d24fe80b38cd4b5c32336aa
Marcos César <marcosesares@gmail.com>
2022-09-06 09:09:56 -0300

    first commit

---
+++ curso-c-sharp-test/KataTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace curso_c_sharp.CodeWars
+{
+  [TestFixture]
+  public class KataTest
+  {
+    [Test]
+    public void KataTests()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(Kata.ToCamelCase("the_stealth_warrior"), Is.EqualTo("theStealthWarrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+        Assert.That(Kata.ToCamelCase("The-Stealth-Warrior"), Is.EqualTo("TheStealthWarrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+      });
+    }
+
+    [Test]
+    public void KataTestSelect()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(Kata.ToCamelCaseSelect("the_stealth_warrior"), Is.EqualTo("theStealthWarrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+        Assert.That(Kata.ToCamelCaseSelect("The-Stealth-Warrior"), Is.EqualTo("TheStealthWarrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+      });
+    }
+
+    [Test]
+    public void KataTestReplace()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(Kata.ToCamelCaseSelect("the_stealth_warrior"), Is.EqualTo("theStealthWarrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+        Assert.That(Kata.ToCamelCaseSelect("The-Stealth-Warrior"), Is.EqualTo("TheStealthWarrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+      });
+    }
+
+    [Test]
+    public void GetVowelCount()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(Kata.GetVowelCount("abracadabra"), Is.EqualTo(5), "Nope!");
+        Assert.That(Kata.GetVowelCount("comida"), Is.EqualTo(3), "Nope!");
+        Assert.That(Kata.GetVowelCount("abraço"), Is.EqualTo(3), "Nope!");
+      });
+    }
+  }
+}
+
---
+++ curso-c-sharp/CodeWars/Kata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace curso_c_sharp.CodeWars
+{
+
+  public class Kata
+  {
+    private const int EndIndex = 1;
+
+    public static string ToCamelCase(string str)
+    {
+      string result = string.Empty;
+      if (!string.IsNullOrEmpty(str))
+      {
+        string[] splitStr = str.Split('_','-');
+        result = splitStr[0];
+        for (int i = 1; i < splitStr.Length; i++)
+        {
+          result += string.Concat(splitStr[i][..EndIndex].ToUpper(), splitStr[i][EndIndex..]);
+        }
+      }
+      return result;
+    }
+
+    public static string ToCamelCaseSelect(string str)
+    {
+      return string.Concat(str.Split('-', '_').Select((s, i) => i > 0 ? char.ToUpper(s[0]) + s.Substring(1) : s));
+    }
+
+    public static string ToCamelCaseReplace(string str)
+    {
+      return Regex.Replace(str, @"[_-](\w)", m => m.Groups[1].Value.ToUpper());
+    }
+
+    public static int GetVowelCount(string str)
+    {
+      string vowels = "aeiou";
+      int result = str.Where(s => vowels.Contains(s)).Count();
+      return result;
+    }
+
+    public static int GetVowelCount2(string str)
+    {
+      return str.Count(i => "aeiou".Contains(i));
+    }
+
+    public static int GetVowelCount3(string str)
+    {
+      return (Regex.Matches(str, @"[aeiouAEIOU]")).Count;
+    }
+  }
+}
