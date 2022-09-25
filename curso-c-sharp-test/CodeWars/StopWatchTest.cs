5638251692d3f6c50dabdce8cdfb55e8275c3532
Marcos César <marcosesares@gmail.com>
2022-09-24 21:12:47 -0300

    Adding StopWatch exercise.

---
+++ curso-c-sharp-test/CodeWars/StopWatchTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using curso_c_sharp.CodeWars;
+
+namespace curso_c_sharp_test.CodeWars
+{
+  [TestFixture]
+  public class StopWatchTest
+  {
+    public StopWatchTest()
+    {
+    }
+
+    [Test]
+    public void StartStopWatch()
+    {
+      StopWatch.Start();
+      System.Threading.Thread.Sleep(2000);
+      StopWatch.Stop();
+      Assert.That(StopWatch.Duration().Seconds, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void StartWatch()
+    {
+      StopWatch.Start();
+      var exception = Assert.Throws<ArgumentException>(() => StopWatch.Duration());
+      Assert.That(exception.Message, Is.EqualTo("Stop watch must be started and stopped at least once to calculate a duration."));
+    }
+
+    [Test]
+    public void TryToStopWatch()
+    {
+      var exception = Assert.Throws<ArgumentException>(() => StopWatch.Stop());
+      Assert.That(exception.Message, Is.EqualTo("Stop watch is not started."));
+    }
+
+    [Test]
+    public void TryDuration()
+    {
+      var exception = Assert.Throws<ArgumentException>(() => StopWatch.Duration());
+      Assert.That(exception.Message, Is.EqualTo("Stop watch must be started and stopped at least once to calculate a duration."));
+    }
+
+    [Test]
+    public void TryWithoutStopWatch()
+    {
+      StopWatch.Start();
+      var exception = Assert.Throws<ArgumentException>(() => StopWatch.Duration());
+      Assert.That(exception.Message, Is.EqualTo("Stop watch must be started and stopped at least once to calculate a duration."));
+    }
+
+  }
+}
---
+++ curso-c-sharp/CodeWars/StopWatch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace curso_c_sharp.CodeWars
+{
+  /*
+  <summary>
+    Design a class called Stopwatch. The job of this class is to simulate a stopwatch.
+    It should provide two methods: Start and Stop.
+    We call the start method first, and the stop method next. 
+    Then we ask the stopwatch about the duration between start and stop.
+    Duration should be a value in TimeSpan.
+    Display the duration on the console. 
+    We should also be able to use a stopwatch multiple times.
+    So we may start and stop it and then start and stop it again.
+    Make sure the duration value each time is calculated properly. 
+    We should not be able to start a stopwatch twice in a row (because that may overwrite the initial start time).
+    So the class should throw an InvalidOperationException if its started twice.
+  <summary>
+  */
+  public class StopWatch
+  {
+    private static DateTime _Start;
+    private static DateTime _Stop;
+    private static bool _Started;
+    private static bool _Stopped;
+
+    private StopWatch()
+    {
+    }
+
+    public static void Start()
+    {
+      if (_Started)
+      {
+        throw new ArgumentException("Stop watch is already started.");
+      }
+      _Started = true;
+      _Start = DateTime.Now;
+    }
+
+    public static void Stop()
+    {
+      if (!_Started)
+      {
+        throw new ArgumentException("Stop watch is not started.");
+      }
+      _Started = false;
+      _Stopped = true;
+      _Stop = DateTime.Now;
+    }
+
+    public static TimeSpan Duration()
+    {
+      if (_Started || !_Stopped)
+      {
+        throw new ArgumentException("Stop watch must be started and stopped at least once to calculate a duration.");
+      }
+      var duration = _Stop - _Start;
+      Console.WriteLine("Duration: {0}", duration);
+      return duration;
+    }
+  }
+}
+
